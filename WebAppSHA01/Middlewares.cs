284ceaa51f37c77852582944884c09a921036cde
Shalini Reddy Annam <shaannam@deloitte.com>
2022-04-11 09:56:24 -0500

    changes

---
+++ WebAppSHA01/Middlewares.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace WebAppSHA01
+{
+    public static class Middlewares
+    {
+        public static void UseOnUrl(this IApplicationBuilder app, string url, RequestDelegate action)
+        {
+            app.Use(next => async context =>
+            {
+                if (context.Request.Path.Value == url)
+                    await action(context);
+                else
+                    await next(context);
+            });
+        }
+    }
+}
---
+++ WebAppSHA01/Startup.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebAppSHA01
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.Use(next => async context =>
+            {
+
+                await next(context); //let the other middleware work
+                //now I will add the footer
+                await context.Response.WriteAsync("<hr/><p>&copy; http://conceptarchitect.in</p>");
+
+            });
+
+            app.Use(next => async context =>
+            {
+                //display a common message
+                await context.Response.WriteAsync($"<h1>Book's Web</h1><hr/>");
+                //let other middleware do whatever they want.
+                await next(context);  //pass control to the next middleware
+            });
+
+
+            app.UseOnUrl("/long-running", async context =>
+            {
+                await context.Response.WriteAsync($"Long running The work started -  {DateTime.Now.ToLongTimeString()}");
+                await Task.Delay(2000);
+                await context.Response.WriteAsync($"Long running workd finished -  {DateTime.Now.ToLongTimeString()}");
+
+            });
+
+            app.UseOnUrl("/date", async context =>
+            {
+                await context.Response.WriteAsync($"Date is : {DateTime.Now.ToLongDateString()}");
+            });
+
+            app.UseOnUrl("/time", async context =>
+            {
+                await context.Response.WriteAsync($"Time now is {DateTime.Now.ToLongTimeString()}");
+
+            });
+
+
+
+
+            app.Use(next => async context =>
+            {
+                //this middleware just logs the information
+                Console.WriteLine($"Received {context.Request.Method} {context.Request.Path} ");
+                //there is not visible output here.
+                await next(context);  //pass control to the next middleware
+            });
+
+
+            app.Use(next => {
+
+                RequestDelegate thisMiddleware = async context =>
+                {
+                    //perform whatever you want to
+                    await context.Response.WriteAsync($"Handled {context.Request.Path}");
+                };
+
+                return thisMiddleware;
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"Time: {DateTime.Now.ToLongTimeString()}");
+            });
+
+
+            //unreachable code
+            app.Run(new RequestDelegate(Greet));
+        }
+
+        private async Task Greet(HttpContext context)
+        {
+            await context.Response.WriteAsync($"Hello World to {context.Request.Path} ");
+        }
+    }
+}
