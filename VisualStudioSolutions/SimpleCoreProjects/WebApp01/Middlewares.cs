ae11b0ead7f884984bc3c2113cc5070fd9f92a44
M Bhaskar <mbhaskar41@gmail.com>
2022-04-12 09:28:03 +0530

    [Asssignement1.1#updated]

--- VisualStudioSolutions/SimpleCoreProjects/WebApp01/Middlewares.cs
+++ VisualStudioSolutions/SimpleCoreProjects/WebApp01/Middlewares.cs
@@ -11,7 +11,7 @@ namespace WebApp01
     {
         public static void UseOnUrl(this IApplicationBuilder app, string url, RequestDelegate action)
         {
-            app.Use( next => async context =>
+            app.Use(next => async context =>
             {
                 if (context.Request.Path.Value == url)
                     await action(context);
@@ -19,5 +19,18 @@ namespace WebApp01
                     await next(context);
             });
         }
+        public static void ErrorandStatConfiguration(this IApplicationBuilder app, Action<string, string> action)
+        {
+            app.Use(next => async context =>
+            {
+                await next(context);
+                if (context.Response.StatusCode == 404)
+                    action($"{context.Request.Path.Value}{context.Request.QueryString.Value}", "NotFound");
+                else
+                    action($"{context.Request.Path.Value}{context.Request.QueryString.Value}", "Found");
+
+            });
+        }
     }
 }
+
--- VisualStudioSolutions/SimpleCoreProjects/WebApp01/Startup.cs
+++ VisualStudioSolutions/SimpleCoreProjects/WebApp01/Startup.cs
@@ -4,12 +4,15 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace WebApp01
 {
     public class Startup
     {
+        private Dictionary<string, int> StatUrlsCount = new Dictionary<string, int>();
+        private Dictionary<string, int> NotFoundUrls = new Dictionary<string, int>();
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,7 +23,7 @@ namespace WebApp01
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
-            app.Use( next => async context =>
+            app.Use(next => async context =>
             {
 
                 await next(context); //let the other middleware work
@@ -38,9 +41,9 @@ namespace WebApp01
             });
 
 
-            app.UseOnUrl("/long-running",  async context =>
+            app.UseOnUrl("/long-running", async context =>
             {
-                
+
                 await context.Response.WriteAsync("Long running The work started...");
                 await Task.Delay(2000);
                 await context.Response.WriteAsync("Long running workd finished...");
@@ -52,19 +55,31 @@ namespace WebApp01
                 await context.Response.WriteAsync($"Date is : {DateTime.Now.ToLongDateString()}");
             });
 
-            Middlewares.UseOnUrl(app,"/time", async context =>
+            Middlewares.UseOnUrl(app, "/time", async context =>
             {
-                  await context.Response.WriteAsync($"Time now is {DateTime.Now.ToLongTimeString()}");
+                await context.Response.WriteAsync($"Time now is {DateTime.Now.ToLongTimeString()}");
+
+            });
+            app.ErrorandStatConfiguration((url, err) => AddUrlsToDictionary(url, err));
+
+            app.UseOnUrl("/stats", async context =>
+            {
+                await context.Response.WriteAsync("<h1>URL's </h1> <br>");
+                await PrintUrls(context, StatUrlsCount);
 
             });
 
+            app.UseOnUrl("/404", async context =>
+            {
+                await context.Response.WriteAsync("<h1>Unhandled Urls</h1> <br>");
+                await PrintUrls(context, NotFoundUrls);
+            });
 
-           
 
-            app.Use (next => async context =>
+            app.Use(next => async context =>
             {
                 //this middleware just logs the information
-                Console.WriteLine($"Received {context.Request.Method} {context.Request.Path} " );
+                Console.WriteLine($"Received {context.Request.Method} {context.Request.Path} ");
                 //there is not visible output here.
                 await next(context);  //pass control to the next middleware
             });
@@ -95,6 +110,36 @@ namespace WebApp01
         private async Task Greet(HttpContext context)
         {
             await context.Response.WriteAsync($"Hello World to {context.Request.Path} ");
+        }
+        private async Task PrintUrls(HttpContext context, Dictionary<string, int> stats)
+        {
+            foreach (var url in stats)
+            {
+                await context.Response.WriteAsync($"Url:{url.Key} Visited Count {url.Value} <br>");
+            }
+        }
+        private void AddUrlsToDictionary(string url, string message)
+        {
+            if (message == "Found")
+            {
+                UrlsAddingorUpdating(StatUrlsCount, url);
+            }
+            else
+            {
+                UrlsAddingorUpdating(NotFoundUrls, url);
+            }
+        }
+
+        private void UrlsAddingorUpdating(Dictionary<string, int> statsUrls, string url)
+        {
+            if (statsUrls.ContainsKey(url))
+            {
+                statsUrls[url] += 1;
+            }
+            else
+            {
+                statsUrls.Add(url, 1);
+            }
         }
     }
 }
