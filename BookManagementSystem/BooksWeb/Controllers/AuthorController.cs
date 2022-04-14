9303cfc941f721bd957ee4dd7c99484a46156b24
Mudda Rakesh Kumar Redddy <mredddy@deloitte.com>
2022-04-14 17:40:49 +0530

    message

---
+++ BookManagementSystem/BooksWeb/Controllers/AuthorController.cs
@@ -0,0 +1,31 @@
+﻿using ConceptArchitect.BookManagement;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksWeb.Controllers
+{
+    public class AuthorController:Controller
+    {
+        IAuthorService authorService;
+
+        public AuthorController(IAuthorService authorService)
+        {
+            this.authorService = authorService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var authors =await authorService.GetAllAuthors();
+            return View(authors);
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            var author = await authorService.GetAuthorById(id);
+            return View(author);
+        }
+    }
+}
---
+++ BookManagementSystem/ConceptArchitect.BookManagement/IAuthorService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConceptArchitect.BookManagement
+{
+    public interface IAuthorService
+    {
+        Task AddAuthor(Author author);
+        Task<IList<Author>> GetAllAuthors();
+        Task<Author> GetAuthorById(string id);
+    }
+}
